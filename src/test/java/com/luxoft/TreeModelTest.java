f840e73e507ff0541ba8fdccd17c9d72e7285eac
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2017-03-25 10:24:56 +0300

    tree model with person.json

--- src/test/java/com/luxoft/TreeModelTest.java
+++ src/test/java/com/luxoft/TreeModelTest.java
@@ -2,20 +2,40 @@ package com.luxoft;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class TreeModelTest {
 
+    @Before
+    public void init() {
+
+    }
+
     @Test
-    public void test() throws IOException {
+    public void test() throws IOException, URISyntaxException {
+
+        String content = getFileContent("person.json");
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode rootNode = mapper.readValue("{\"message\":\"Hi\",\"place\":{\"name\":\"World!\"}}", JsonNode.class);
-        String message = rootNode.get("message").asText(); // get property message
-        JsonNode childNode =  rootNode.get("place"); // get object Place
-        String place = childNode.get("name").asText(); // get property name
-        System.out.println(message + " " + place); // print "Hi World!"
+
+        JsonNode rootNode = mapper.readValue(content, JsonNode.class);
+        String lastName = rootNode.get("lastName").asText();
+
+        JsonNode childNode = rootNode.get("address");
+        String city = childNode.get("city").asText();
+
+        System.out.println(lastName + " " + city);
+    }
+
+    private String getFileContent(String name) throws URISyntaxException, IOException {
+        URI uri = Thread.currentThread().getContextClassLoader().getResource(name).toURI();
+        return new String(Files.readAllBytes(Paths.get(uri)));
     }
 
 }
